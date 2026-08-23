61de62d8a29c931a197d11761748b4e124386451
teloanton56 <ebayteloanton@gmail.com>
2026-08-24 00:38:15 +0200

    Bump app to v0.25 decision engine

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ from app.services.ebay import EbayClient
 from app.services.scheduler import start_scheduler, stop_scheduler
 from app.config import get_settings
 
-VERSION = "0.24.0"
+VERSION = "0.25.0"
 BRAND_REV = "ops-knot-1"
 
 
